da50c2aec7924e5cef59dcfa62872af0906ab901
akshay14103058 <akshayjeengar@gmail.com>
2017-05-16 17:01:25 +0530

    Update aithmaticop.cpp
    addition features of multipication of 3 numbers and addition of two numbers has been added

--- aithmaticop.cpp
+++ aithmaticop.cpp
@@ -2,9 +2,9 @@
 uisng namespace std;
 int main()
 {
-int a,b;
-cout<<"enter two number a and b";
-cin>>a,b;
+int a,b,c;
+cout<<"enter two number a and b and c";
+cin>>a>>b>>c;;
 
 int d=a+b;
 cout<<"sum of two number a and b is:- "<<d;
